5b9a951400daba8741a34683beab0e272ff674b7
fahr-khadija <100168693+fahr-khadija@users.noreply.github.com>
2023-11-15 01:07:06 -0500

    update and clean app.js

--- Flask-API/static/js/app.js
+++ Flask-API/static/js/app.js
@@ -1,135 +1,60 @@
-// Dropdown Menu for Country1
-let dropdownMenu1 = d3.select('#selCountry1');
+// Put json file  in a const URL 
+// I've used the "Country" key directly for the dropdown menu options and selection to adapt to the url 
+//const url = "http://localhost:8000/projectdata_modified.json"; 
+// put the db under static so  flask can take it 
+   const url = '/static/projectdata_modified.json';
 
-// Dropdown Menu for Country2
-let dropdownMenu2 = d3.select('#selCountry2');
-// Put your URL in a URL variable
-const url = 'http://127.0.0.1:5000/api/v1.0/all_data';
-// const url ='http://localhost:8000/projectdata_modified.json';
-// Dummy layout for the bubble chart, customize as needed
-console.log("Hello therer")
-// Fetch the JSON data and console log it
-d3.json(url).then(function(data) {
-  console.log(data);
-});
+ // Create a horizontal bar chart with a dropdown menu to display data for the selected country.
+// Modify the barChart function for better visualization of two selected countries
+// Modify the barChart function to display entries on the y-axis and values on the x-axis with 2 colors for the countries selected 
 
-const bubbleLayout = {
-  xaxis: { title: 'Attribute' },
+function barChart(selectedCountries) {
+ // Fetch the JSON data and console log it
+  d3.json(url).then((data) => {
+     console.log(`Data:`, data);
+    let countryDataList = data.projectdata;
+    let selectedCountriesData = countryDataList.filter((countryData) =>
+      selectedCountries.includes(countryData.country)
+    );
+
+    let trace = selectedCountriesData.map((selectedCountryData, index) => ({
+      x: Object.values(selectedCountryData).slice(1, 11).reverse(), // Use values for x-axis
+      y: Object.keys(selectedCountryData).slice(1, 11).reverse(), // Use entries  for y-axis
+      type: 'bar',
+      marker: {
+        // add Blue for the first country, red for the second
+        color: index === 0 ? 'rgba(55, 128, 191, 0.7)' : 'rgba(255, 0, 0, 0.7)', 
+      },
+      name: selectedCountryData.country,
+      orientation: 'h',
+    }));
+
+    const layout = {
+      title: 'Bar Chart visualisation',
+      xaxis: {
+        title: 'X-Axis entries values',
+      },
+      yaxis: {
+        title: 'Y-Axis entries names',
+        automargin: true,
+      },
+    };
+    Plotly.newPlot('barChart', trace, layout);
+  });
 };
 
-// Modify the barChart function for better visualization of two selected countries
-// Modify the barChart function to display entries on the x-axis and values on the y-axis
-function barChart(selectedCountries) {
-    d3.json(url).then((data) => {
-      let countryDataList = data.projectdata;
-      let selectedCountriesData = countryDataList.filter((countryData) =>
-        selectedCountries.includes(countryData.Country)
-      );
-  
-      let trace = selectedCountriesData.map((selectedCountryData, index) => ({
-        x: Object.values(selectedCountryData).slice(1, 11).reverse(), // Use values for x-axis
-        y: Object.keys(selectedCountryData).slice(1, 11).reverse(), // Use keys (entries) for y-axis
-        type: 'bar',
-        marker: {
-          color: index === 0 ? 'rgba(55, 128, 191, 0.7)' : 'rgba(255, 0, 0, 0.7)', // Blue for the first country, red for the second
-        },
-        name: selectedCountryData.Country,
-        orientation: 'h',
-      }));
-  
-      const layout = {
-        title: 'Bar Chart Title',
-        xaxis: {
-          title: 'X-Axis Title',
-        },
-        yaxis: {
-          title: 'Y-Axis Title',
-          automargin: true,
-        },
-      };
-  
-      Plotly.newPlot('barChart', trace, layout);
-    });
-  }
-// Function for scatter plot
-function scatterPlot(selectedCountries) {
-    d3.json(url).then((data) => {
-      let countryDataList = data.projectdata;
-      let selectedCountriesData = countryDataList.filter((countryData) =>
-        selectedCountries.includes(countryData.Country)
-      );
-  
-      let traces = selectedCountriesData.map((selectedCountryData, index) => ({
-        x: Object.values(selectedCountryData).slice(1, 11),
-        y: Object.values(selectedCountryData).slice(11, 21),
-        mode: 'markers',
-        type: 'scatter',
-        name: selectedCountryData.Country,
-        text: Object.values(selectedCountryData).slice(1, 11).map((value, i) => `${Object.keys(selectedCountryData)[i + 1]}: ${value}`),
-        marker: {
-          size: 10,
-          color: index === 0 ? 'rgba(55, 128, 191, 0.7)' : 'rgba(255, 0, 0, 0.7)', // Blue for the first country, red for the second
-        },
-      }));
-  
-      const layout = {
-        title: 'Scatter Plot Title',
-        xaxis: {
-          title: 'X-Axis Title',
-        },
-        yaxis: {
-          title: 'Y-Axis Title',
-        },
-      };
-  
-      Plotly.newPlot('scatterPlot', traces, layout);
-    });
-  }
- 
-    // Function to update info based on selected country
-function optionChanged(countryType, selectedCountry) {
-    d3.json(url).then((data) => {
-      let countryDataList = data.projectdata;
-      let selectedCountryData = countryDataList.find((countryData) => countryData.Country === selectedCountry);
-  
-      // Update the HTML elements with the information
-      let infoPanel = d3.select(`#sample-metadata${countryType}`);
-      infoPanel.html('');
-  
-      Object.entries(selectedCountryData).forEach(([key, value]) => {
-        infoPanel.append('h5').text(`${key}: ${value}`);
-      });
-    });
-  }
-  
-  // Listen for changes on the dropdowns
-  dropdownMenu1.on('change', function () {
-    let selectedCountry1 = dropdownMenu1.property('value');
-    let selectedCountry2 = dropdownMenu2.property('value');
-    plot([selectedCountry1, selectedCountry2]);
-  
-    // Update Country1 Info
-    optionChanged('1', selectedCountry1);
-  });
-  
-  dropdownMenu2.on('change', function () {
-    let selectedCountry1 = dropdownMenu1.property('value');
-    let selectedCountry2 = dropdownMenu2.property('value');
-    plot([selectedCountry1, selectedCountry2]);
-  
-    // Update Country2 Info
-    optionChanged('2', selectedCountry2);
-  });
-  
-  
-  // Modify the bubbleChart function for better visualization of two selected countries
+// Function that builds the bubble chart
+// Modify the bubbleChart function for better visualization of two selected countries
   function bubbleChart(selectedCountries) {
+    // Fetch the JSON data 
     d3.json(url).then((data) => {
       let countryDataList = data.projectdata;
       let selectedCountriesData = countryDataList.filter((countryData) =>
-        selectedCountries.includes(countryData.Country)
+        selectedCountries.includes(countryData.country)
       );
-  
+      const bubbleLayout = {
+        xaxis: { title: 'Attribute' },
+       };  
       let trace = selectedCountriesData.map((selectedCountryData, index) => ({
         x: Object.keys(selectedCountryData).slice(1),
         y: Object.values(selectedCountryData).slice(1),
@@ -137,91 +62,154 @@ function optionChanged(countryType, selectedCountry) {
         mode: 'markers',
         marker: {
           size: 100,
+          // add Blue for the first country, red for the second
           color: index === 0 ? 'rgba(55, 128, 191, 0.7)' : 'rgba(255, 0, 0, 0.7)', // Blue for the first country, red for the second
         },
-        name: selectedCountryData.Country,
+        name: selectedCountryData.country,
       }));
-  
-      Plotly.newPlot('bubbleChart', trace, bubbleLayout);
+       // Plot the bubble chart with the selected country
+       Plotly.newPlot('bubbleChart', trace, bubbleLayout);
+
     });
   }
-  
-  // Other functions and initialization remain unchanged
-  
-// Function to plot all charts when we have new selections
+
+// Function for scatter plot
+function scatterPlot(selectedCountries) {
+  d3.json(url).then((data) => {
+    let countryDataList = data.projectdata;
+    let selectedCountriesData = countryDataList.filter((countryData) =>
+      selectedCountries.includes(countryData.country)
+    );
+
+    let traces = selectedCountriesData.map((selectedCountryData, index) => ({
+      x: Object.values(selectedCountryData).slice(1, 11),
+      y: Object.values(selectedCountryData).slice(11, 21),
+      mode: 'markers',
+      type: 'scatter',
+      name: selectedCountryData.country,
+      text: Object.values(selectedCountryData).slice(1, 11).map((value, i) => `${Object.keys(selectedCountryData)[i + 1]}: ${value}`),
+      marker: {
+        size: 10,
+        color: index === 0 ? 'rgba(55, 128, 191, 0.7)' : 'rgba(255, 0, 0, 0.7)', // Blue for the first country, red for the second
+      },
+    }));
+
+    const layout = {
+      title: 'Scatter Plot visualisation',
+      xaxis: {
+        title: 'X-Axis entries values',
+      },
+      yaxis: {
+        title: 'Y-Axis entries names',
+      },
+    };
+    // Plot the scatterPlot  with the selected countries
+    Plotly.newPlot('scatterPlot', traces, layout);
+  });
+}
+// function "demog" to filter data for selected country and update the html accordingly based on the selected country
+function demog(selectedCountries) {
+  // Fetch the JSON data and console log it
+  d3.json(url).then((data) => {
+    console.log(`Data:`, data);
+
+    let countryDataList = data.projectdata;
+
+    // Filter data for selected countries
+    let selectedCountriesData = countryDataList.filter((countryData) =>
+      selectedCountries.includes(countryData.country)
+    );
+
+    // Update the HTML elements with the information
+    selectedCountriesData.forEach((selectedCountryData, index) => {
+      let infoPanel = d3.select(`#sample-metadata${index + 1}`);
+      infoPanel.html('');
+
+      // Iterate through the selectedCountryData object and console for each country
+      Object.entries(selectedCountryData).forEach(([key, value]) => {
+        infoPanel.append('h5').text(`${key}: ${value}`);
+      });
+    });
+
+    // Log the entries array to the  console
+    console.log(selectedCountriesData);
+  });
+
+}
+ //  //  //  //  // Dropdown Menu //  //  //  //  //
+// put a variable for Dropdown Menu for Country1
+let dropdownMenu1 = d3.select('#selCountry1');
+
+// put a variable for Dropdown Menu for Country2 
+let dropdownMenu2 = d3.select('#selCountry2');
+
+// Fetch the JSON data and console log it
+d3.json(url).then(function (data) {
+  let countryList = data.projectdata.map((countryData) => countryData.country);
+
+// Populate Country1 dropdown
+countryList.forEach((country) => {
+  dropdownMenu1.append('option').text(country).property('value', country);
+});
+
+// Populate Country2 dropdown
+countryList.forEach((country) => {
+  dropdownMenu2.append('option').text(country).property('value', country);
+});
+
+// Select initial countries based on dropdown selections
+let initialCountry1 = dropdownMenu1.property('value');
+let initialCountry2 = dropdownMenu2.property('value');
+ // Pass the initial selections as an array
+ plot([initialCountry1, initialCountry2]);
+});
+
+// Listen for changes on the dropdowns menu for Country1
+dropdownMenu1.on('change', function () {
+  let selectedCountry1 = dropdownMenu1.property('value');
+  let selectedCountry2 = dropdownMenu2.property('value');
+  plot([selectedCountry1, selectedCountry2]);
+
+  // Update Country1 Info
+  optionChanged('1', selectedCountry1);
+});
+// Listen for changes on the dropdowns menu for Country2
+dropdownMenu2.on('change', function () {
+  let selectedCountry1 = dropdownMenu1.property('value');
+  let selectedCountry2 = dropdownMenu2.property('value');
+  plot([selectedCountry1, selectedCountry2]);
+
+  // Update Country2 Info
+  optionChanged('2', selectedCountry2);
+});
+ //  //  //  //  // 
+
+// Function to update info based on selected country
+function optionChanged(countryType, selectedCountry) {
+  d3.json(url).then((data) => {
+    let countryDataList = data.projectdata;
+    let selectedCountryData = countryDataList.find((countryData) => countryData.country === selectedCountry);
+
+    // Update the HTML elements with the information
+    let infoPanel = d3.select(`#sample-metadata${countryType}`);
+    infoPanel.html('');
+
+    Object.entries(selectedCountryData).forEach(([key, value]) => {
+      infoPanel.append('h5').text(`${key}: ${value}`);
+    });
+  });
+}
+
+// Function to plot all charts when we have new selections for country 
 function plot(selectedCountries) {
   console.log(selectedCountries);
   demog(selectedCountries);
   barChart(selectedCountries);
   bubbleChart(selectedCountries);
   scatterPlot(selectedCountries);
-  createMap(selectedCountries);
+  //createMap(selectedCountries);
 }
 
-// Demographic function "demog"
-function demog(selectedCountries) {
-    // Fetch the JSON data and console log it
-    d3.json(url).then((data) => {
-      console.log(`Data:`, data);
-  
-      let countryDataList = data.projectdata;
-  
-      // Filter data for selected countries
-      let selectedCountriesData = countryDataList.filter((countryData) =>
-        selectedCountries.includes(countryData.Country)
-      );
-  
-      // Update the HTML elements with the information
-      selectedCountriesData.forEach((selectedCountryData, index) => {
-        let infoPanel = d3.select(`#sample-metadata${index + 1}`);
-        infoPanel.html('');
-  
-        // Iterate through the selectedCountryData object
-        Object.entries(selectedCountryData).forEach(([key, value]) => {
-          infoPanel.append('h5').text(`${key}: ${value}`);
-        });
-      });
-  
-      // Log the entries array
-      console.log(selectedCountriesData);
-    });
-
-  }
-    
-// Fetch the JSON data and console log it
-d3.json(url).then(function (data) {
-  let countryList = data.projectdata.map((countryData) => countryData.Country);
-
-  // Populate Country1 dropdown
-  countryList.forEach((country) => {
-    dropdownMenu1.append('option').text(country).property('value', country);
-  });
-
-  // Populate Country2 dropdown
-  countryList.forEach((country) => {
-    dropdownMenu2.append('option').text(country).property('value', country);
-  });
-
-  // Select initial countries based on dropdown selections
-  let initialCountry1 = dropdownMenu1.property('value');
-  let initialCountry2 = dropdownMenu2.property('value');
-
-  plot([initialCountry1, initialCountry2]); // Pass the initial selections as an array
-});
-
-
-// Listen for changes on the dropdowns
-dropdownMenu1.on('change', function () {
-  let selectedCountry1 = dropdownMenu1.property('value');
-  let selectedCountry2 = dropdownMenu2.property('value');
-  plot([selectedCountry1, selectedCountry2]);
-});
-
-dropdownMenu2.on('change', function () {
-  let selectedCountry1 = dropdownMenu1.property('value');
-  let selectedCountry2 = dropdownMenu2.property('value');
-  plot([selectedCountry1, selectedCountry2]);
-});
 
 // Initiation function
 function init() {
@@ -230,60 +218,22 @@ function init() {
 
   // Fetch the JSON data and console log it
   d3.json(url).then(function (data) {
-    let countryList = data.projectdata.map((countryData) => countryData.Country);
+    let countryList = data.projectdata.map((countryData) => countryData.country);
 
     countryList.forEach((country) => {
       dropdownMenu.append('option').text(country).property('value', country);
     });
-
-    let initialCountries = countryList.slice(0, 2); // Select the first two countries
-    plot(initialCountries); // Pass the initial selection as an array
+// Select the first two countries algeria and angola 
+    let initialCountries = countryList.slice(0, 2); 
+    // Plot the initial selection as an array
+    plot(initialCountries); 
   });
 
   dropdownMenu.on('change', function () {
     let selectedCountries = d3.select('#selDataset').selectAll('option:checked').nodes().map(option => option.value);
     plot(selectedCountries);
   });
-
-
-// Function to create a map
-function createMap(selectedCountries) {
-  // Fetch the JSON data and console log it
-  d3.json(url).then((data) => {
-    let countryDataList = data.projectdata;
-    let selectedCountriesData = countryDataList.filter((countryData) =>
-      selectedCountries.includes(countryData.Country)
-    );
-  });
-    // Initialize the map
-      let map = L.map('interactiveMap').setView([0, 0], 2);
-
-// Add a tile layer to the map
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-}).addTo(map);
-
-// Add a marker for testing
-L.marker([0, 0]).addTo(map);
-    // Add a tile layer (you can customize the tile layer URL)
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© OpenStreetMap contributors',
-    }).addTo(map);
-
-    // Add markers for selected countries
-    selectedCountriesData.forEach((selectedCountryData) => {
-      let lat = parseFloat(selectedCountryData.Latitude);
-      let lon = parseFloat(selectedCountryData.Longitude);
-
-      if (!isNaN(lat) && !isNaN(lon)) {
-        L.marker([lat, lon])
-          .addTo(map)
-          .bindPopup(selectedCountryData.Country)
-          .openPopup();
-      }
-    });
-  };
-}
+};
 
 
 init();
