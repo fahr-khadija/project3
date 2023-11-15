9a088ad52a246f86be5794e7820a10548245a9b5
daniklafrance <daniklafrance@hotmail.com>
2023-11-15 11:33:56 -0500

    save

--- Flask-API/static/js/test.js
+++
@@ -1,9 +0,0 @@
-const url = 'http://127.0.0.1:5000/all_data';
-
-const dataPromise = d3.json(url);
-console.log("Data Promise: ", dataPromise);
-
-// Fetch the JSON data and console log it
-d3.json(url).then(function(data) {
-  console.log(data);
-});
