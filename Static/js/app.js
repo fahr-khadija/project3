cb6f3e2c618f970177660b5044bca8e525b69358
daniklafrance <daniklafrance@hotmail.com>
2023-11-16 18:32:16 -0500

    update

--- Static/js/app.js
+++ Static/js/app.js
@@ -137,39 +137,39 @@ function bubbleChart(selectedCountries) {
   });
 }
 
-function piechart(selectedCountries) {
+function pieChart(selectedCountries) {
   // Fetch the JSON data and console log it
    d3.json(url).then((data) => {
       console.log(`Data:`, data);
-     let countryDataList = data.projectdata;
-     let selectedCountriesData = countryDataList.filter((countryData) =>
-       selectedCountries.includes(countryData.country)
-     );
+      let countryDataList = data.projectdata;
+      let selectedCountriesData = countryDataList.filter((countryData) =>
+        selectedCountries.includes(countryData.country)
+      );
   
-     let trace = selectedCountriesData.map((selectedCountryData, index) => ({
-       x: Object.values(selectedCountryData).slice(1, 11).reverse(), // Use values for x-axis
-       y: Object.keys(selectedCountryData).slice(1, 11).reverse(), // Use entries  for y-axis
-       type: 'pie',
-       marker: {
+      let trace = selectedCountriesData.map((selectedCountryData, index) => ({
+          x: Object.values(selectedCountryData).slice(1, 11).reverse(), // Use values for x-axis
+          y: Object.keys(selectedCountryData).slice(1, 11).reverse(), // Use entries  for y-axis
+          type: 'pie',
+          marker: {
          // add Blue for the first country, red for the second
-         color: index === 0 ? 'rgba(55, 128, 191, 0.7)' : 'rgba(255, 0, 0, 0.7)', 
-       },
-       name: selectedCountryData.country,
-       orientation: 'h',
+              color: index === 0 ? 'rgba(55, 128, 191, 0.7)' : 'rgba(255, 0, 0, 0.7)', 
+          },
+          name: selectedCountryData.country,
+          orientation: 'h',
      }));
   
-     const layout = {
-       title: 'Pie chart  visualisation',
-       xaxis: {
-         title: 'X-Axis entries values',
-       },
-       yaxis: {
-         title: 'Y-Axis entries names',
-         automargin: true,
-       },
-     };
-     Plotly.newPlot('pie', trace, layout);
-   });
+      const layout = {
+          title: 'Pie chart  visualisation',
+          xaxis: {
+              title: 'X-Axis entries values',
+          },
+          yaxis: {
+              title: 'Y-Axis entries names',
+              automargin: true,
+          },
+      };
+      Plotly.newPlot('pie', trace, layout);
+  });
 }
 
 
@@ -298,7 +298,7 @@ function optionChanged(countryType, selectedCountry) {
       infoPanel.append('h5').text(`${key}: ${value}`);
     });
   });
-}
+};
 
 // Function to plot all charts when we have new selections for country 
 function plot(selectedCountries) {
@@ -309,8 +309,8 @@ function plot(selectedCountries) {
   barChart3(selectedCountries);
   bubbleChart(selectedCountries);
   scatterPlot(selectedCountries);
-  piechart(selectedCountries);
-}
+  pieChart(selectedCountries)
+};
 
 
 // Initiation function
