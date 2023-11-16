b9fbf6b01808d3abc498942ef3c8bb13e8cc0f84
daniklafrance <daniklafrance@hotmail.com>
2023-11-16 18:25:06 -0500

    update

--- Static/js/app.js
+++ Static/js/app.js
@@ -137,6 +137,41 @@ function bubbleChart(selectedCountries) {
   });
 }
 
+function piechart(selectedCountries) {
+  // Fetch the JSON data and console log it
+   d3.json(url).then((data) => {
+      console.log(`Data:`, data);
+     let countryDataList = data.projectdata;
+     let selectedCountriesData = countryDataList.filter((countryData) =>
+       selectedCountries.includes(countryData.country)
+     );
+  
+     let trace = selectedCountriesData.map((selectedCountryData, index) => ({
+       x: Object.values(selectedCountryData).slice(1, 11).reverse(), // Use values for x-axis
+       y: Object.keys(selectedCountryData).slice(1, 11).reverse(), // Use entries  for y-axis
+       type: 'pie',
+       marker: {
+         // add Blue for the first country, red for the second
+         color: index === 0 ? 'rgba(55, 128, 191, 0.7)' : 'rgba(255, 0, 0, 0.7)', 
+       },
+       name: selectedCountryData.country,
+       orientation: 'h',
+     }));
+  
+     const layout = {
+       title: 'Pie chart  visualisation',
+       xaxis: {
+         title: 'X-Axis entries values',
+       },
+       yaxis: {
+         title: 'Y-Axis entries names',
+         automargin: true,
+       },
+     };
+     Plotly.newPlot('pie', trace, layout);
+   });
+}
+
 
 // // Function for scatter plot
 // function scatterPlot(selectedCountries) {
@@ -274,7 +309,7 @@ function plot(selectedCountries) {
   barChart3(selectedCountries);
   bubbleChart(selectedCountries);
   scatterPlot(selectedCountries);
-  //createMap(selectedCountries);
+  piechart(selectedCountries);
 }
 
 
