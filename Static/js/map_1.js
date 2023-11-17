6a5457cdd4b3fe21b576939f6a8f40f8866b5194
Yosieph <yfissuh@yahoo.com>
2023-11-16 23:54:38 -0500

    new

--- Static/js/map_1.js
+++ Static/js/map_1.js
@@ -1,31 +1,52 @@
-const url = 'http://127.0.0.1:5000/all_data';
+const url = "http://127.0.0.1:5000/all_data";
 
-// // Perform a GET request to the query URL/
+// // Perform a GET request to the query URL
 // d3.json(url).then(function (projectdata) {
 //     // Once we get a response, send the data.features object to the createFeatures function.
-//     createFeatures(projectdata.features);
+//     init(projectdata);
 //   });
   
-let labels = Object.keys(data.url);
 
-// Extract values from the 
-let pievalua = Object.values(data.url);
+// // Display the default plot
+//  function init(projectdata) {
+//   console.log("Hello David", projectdata)
+//   // let labels = projectdata.map(item => item.projectdata['country']); 
+//   // let pieValues = projectdata.map(item => item.projectdata['Electricity from renewables (TWh)']);
 
-// Display the default plot
-function init() {
-  let plotData = [{
-    values: pievaluavalua,
-    labels: labels,
-    type: "pie"
-  }];
 
-  let layout = {
-    height: 600,
-    width: 800
-  };
+//   let plotData = [{
+//     values: pieValues,
+//     labels: labels,
+//     type: "pie"
+//   }];
+//   let layout = {
+//     height: 600,
+//     width: 800
+//   };
 
-  Plotly.newPlot("pie", plotData, layout);
-}
+//   Plotly.newPlot("piechart", plotData, layout);
+// }
 
-// Call the init function to display the pie chart
-init();
+// // Call the init function to display the pie chart
+// init();
+
+function piechart(selectedCountries){
+  
+  d3.json(url).then(function (data) {
+    let countryList = data.projectdata.map((countryData) => countryData.country);
+    let selectedCountriesData = countryList.filter((countryData) =>
+    selectedCountries.includes(countryData.country)
+    );
+
+    let plotData = [{
+      values: countryList,
+      labels: selectedCountriesData,
+      type:"pie"
+    }];
+    let layout = {
+      hieght: 600,
+      width: 800,
+    };
+    Plotly.newPlot("piechart", plotData, layout);
+  });
+};
